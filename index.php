7ad3638931eacbc97bfa9aecd6bb7a67f3c45b8a
Ваше имя < >
2025-03-17 13:56:19 +0500

    практически сделана 6

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	// Подключаем header.php
+	include 'header.php';
+?>
+
+<main>
+    <p style="font-size: 20px; line-height: 1.5">Это главная страница.</p>
+</main>
+
+<?php
+	// Подключаем footer.php
+	include 'footer.php';
+?>
